8366461b23a0ccada67b89938b5f19c2de8e8f03
vincobuh <vincobuh@gmail.com>
2017-08-01 22:41:44 +0100

    CA4

--- CA4/R Script Calculator.R
+++ CA4/R Script Calculator.R
@@ -3,34 +3,94 @@
 # Squareroot, Cube, Square, Cuberoot and Factorial
 
 
-add <- function(x, y) {
-  return(x + y)
+Add <- function(num1, num2) {
+  return(num1 + num2)
 }
 
-subtract <- function(x, y) {
-  return(x - y)
+Subtract <- function(num1, num2) {
+  return(num1 - num2)
 }
 
-multiply <- function(x, y) {
-  return(x * y)
+Multiply <- function(num1, num2) {
+  return(num1 * num2)
 }
 
-divide <- function(x, y) {
-  return(x / y)
+Divide <- function(num1, num2) {
+  return(num1 / num2)
 }
 
-# take input from the user
+Exponent <- function(num1, num2) {
+   if (num1 == 0 && num2 == 0)
+      {return("Error!!")}
+   if (num1 == 0)
+      {return(0)}
+   if (num2 == 0)
+      {return(1)}
+   else
+     {return(num1 ** num2)}
+}
+
+Squareroot <- function(num1, num2) {
+  if (num2 == 0.5) 
+    return(num1 ** 0.5)
+  if (num1 < 0)
+    return('NaN')
+  else 
+      return('Error! Please enter(num2 = 0.5)')
+}
+
+
+Cube <- function(num1, num2) {
+   if (num2 == 3)
+     return(num1 ** num2)
+  
+   else
+     return('Error! Please enter(num2 = 3)')
+}
+
+
+Square  <- function(num1, num2) {
+  if (num2 == 2)
+    return(num1 ** num2)
+  else
+    return('Error! Please enter(num2 = 2)')
+}
+  
+Cuberoot <- function(num1, num2) {
+   if (num2 == 1/3)
+     return(num1 ** num2)
+   else
+     return('Error! Please enter(num2 = 1/3)')
+}
+
+Factorial <- function(num1) {
+  if (num1 > 1)
+    return(num1 * factorial(num1-1))
+  else
+    return("Infinity") 
+}
+
+#Take input from the user
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Exponent")
+print("6.Squareroot")
+print("7.Cube")
+print("8.Square")
+print("9.Cuberoot")
+print("10.Factorial, Please enter ONLY num1")
+operation = as.integer(readline(prompt="Enter operation[1/2/3/4/5/6/7/8/9/10]: "))
 
-choice = as.integer(readline(prompt="Enter choice[1/2/3/4]: "))
-num1 = (readline(prompt="Enter first number: "))
-num2 = (readline(prompt="Enter second number: "))
+num1 = as.numeric(readline(prompt="Enter first number: "))
+num2 = is.numeric(readline(prompt="Enter second number: "))
 
-operator <- switch(choice,"+","-","*","/")
-result <- switch(choice, add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2))
+
+operator <- switch(operation,"+","-","*","/","**","**","**","**","**","num1 * factorial(num1-1)")
+result <- switch(operation, Add(num1, num2), Subtract(num1, num2), Multiply(num1, num2), Divide(num1, num2),
+                 Exponent(num1, num2), Squareroot(num1, num2), Cube(num1, num2), Square(num1, num2),
+                 Cuberoot(num1, num2), Factorial(num1))
 
 print(paste(num1, operator, num2, "=", result))
--- CA4/calculator_list.py
+++ CA4/calculator_list.py
@@ -33,11 +33,11 @@ def pythagorean(n):#as generator
 
 pyt = pythagorean(50)
 for v in pyt:
-    print 'Pythagorean as generator\n', v,
+    print 'Pythagorean generator\n', v,
 
 #as list comprehension
 pythagorean = [(x,y,z) for x in range(1,30) for y in range(x,30) for z in range(y,30) if x**2 + y**2 == z**2]	
-print 'Pythagorean as list comprehension: \n', pythagorean
+print 'Pythagorean list comprehension: \n', pythagorean
 
 
 #tests
@@ -50,10 +50,10 @@ print 'Square root: ', sqrt([2, 3, 4, 5])
 print 'Even Values: ', is_even([20, 10, 5, 33, 98])
 print 'FizzBuzz: ', fizzbuzz([15, 34, 30, 100, 9, 20, 60])
 
-################################################################################################################################################
-##########################################
-#       Calculator Functions
-##########################################
+
+
+#Calculator Procedures
+
 
 # Function1: add two lists together using Lambda and Map
 # each element of list1 will be added to the corresponding element of list2
@@ -115,9 +115,9 @@ def generate_squares(mylist):
     for x in mylist:
         yield x**2
             
-##########################################
-#               Calls
-##########################################
+
+
+#Calls
 
 print add_lists([2,5,4],[2,3,0])
 print subtract_lists([2,5,4,3],[2,3,0,4])
